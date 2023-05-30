7271f2373275325e856b8ca245e50649e771de00
JeanFelipe <ermelindo35@hotmail.com>
2023-05-30 18:45:06 -0300

    Aula03

---
+++ Aula03/app.js
@@ -0,0 +1,10 @@
+
+
+// var é utilizado para variaveis de escopo global
+var nome = 'Jean Felipe';
+
+// Let é utilizado para variaveis de escopo local
+let email = jean@hotmail.com;
+
+// const é utilizado tanto no escopo local e goblal, normalmente para conteudos constantes
+const Pi = 3.14;
---
+++ Live01/app.js
@@ -0,0 +1,24 @@
+//console.log("Hello World");
+//console.log("Jean Felipe é um profissional FullStack");
+
+//3 formas de se criar uma variável
+    //let cria uma variavel para utilização no escopo local
+    //var cria uma variave para utilização no escopo global
+    //cont cria uma constante para ser utilizada no projeto
+
+
+let valor1;
+let valor2;
+let valor3;
+let valor4;
+let resultado;
+
+valor1 = 10;
+valor2 = 6;
+valor3 = 15;
+valor4 = 2;
+console.log(typeof(valor1));
+
+resultado = (valor1 + valor2 + valor3 + valor4)/4;
+
+console.log(resultado);
